44956e2636d1f0aeb0dddf442884c1dd54c8be24
eduardoDahmer1 <eduluizdahmer@gmail.com>
2023-03-11 19:28:11 -0300

    commit

--- App/metodoFilter.js
+++ App/metodoFilter.js
@@ -4,8 +4,26 @@ botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
 function filtrarLivros() {
     const elementoBtn = document.getElementById(this.id)
     const categoria = elementoBtn.value
-    let livrosFiltrados = categoria == 'disponivel' ? livros.filter
-    (livro => livro.quantidade > 0) : livros.filter(livro => livro.
-    categoria == categoria)
+    let livrosFiltrados = categoria == 'disponivel' ? filtrarPorDinsponibilidade() : FiltrarPorCategoria(categoria)
     exibirOsLivrosNaTela(livrosFiltrados)
+    if(categoria == 'disponivel'){
+        exibirValorTotalDosLivrosDisponiveisNaTela()
+    }
+}
+
+function FiltrarPorCategoria(categoria) {
+    return livros.filter(livro => livro.
+        categoria == categoria)
+}
+
+function filtrarPorDinsponibilidade() {
+    return livros.filter(livro => livro.quantidade > 0)
+}
+
+function exibirValorTotalDosLivrosDisponiveisNaTela() {
+    elementoComValorTotalDeLivrosDisponiveis.innerHTML = `
+    <div class="livros__disponiveis">
+        <p>Todos os livros disponíveis por R$ <span id="valor">299,00</span></p>
+    </div>
+    `
 }
